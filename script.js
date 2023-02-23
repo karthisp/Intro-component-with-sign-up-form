50bf893a895ac2d5249a0c30cd348b22d47c8bf3
Karthik Sp <karthik.srinivasprasad@gmail.com>
2023-02-23 23:46:23 +0530

    showing error message for empty input boxes

--- script.js
+++ script.js
@@ -5,20 +5,30 @@ introForm.addEventListener('submit', function(e){
     const formContents = this;
     
     if(!formContents.firstName.value) {
-
+        console.log(formContents.firstName)
+        showError(formContents.firstName.parentNode)
     }
     
     if(!formContents.lastName.value) {
-
+        console.log(formContents.lastName)
+        showError(formContents.lastName.parentNode)
     }
 
     if(!formContents.email.value) {
-
+        console.log(formContents.email.parentNode)
+        showError(formContents.email.parentNode)
     }
 
     if(!formContents.password.value) {
-
+        console.log(formContents.password)
+        showError(formContents.password.parentNode)
     }
 
-
 })
+
+function showError(node){
+    const errorImage = node.querySelector('.intro-form-error-icon')
+    const errorReason = node.querySelector('.intro-form-error-msg')
+    errorImage.classList.remove('intro-hide-elm')
+    errorReason.classList.remove('intro-hide-elm')
+}
