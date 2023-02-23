ffe913c626fd652420c9b881e30ab87be943ce46
Karthik Sp <karthik.srinivasprasad@gmail.com>
2023-02-23 23:32:05 +0530

    preventing default form behaviours

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello macha')
+const introForm = document.querySelector('.intro-form');
+
+introForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const formContents = this;
+    
+    if(!formContents.firstName.value) {
+
+    }
+    
+    if(!formContents.lastName.value) {
+
+    }
+
+    if(!formContents.email.value) {
+
+    }
+
+    if(!formContents.password.value) {
+
+    }
+
+
+})
