b7e557742875789404d966ac465c1d468736a648
Karthik Sp <karthik.srinivasprasad@gmail.com>
2023-02-25 14:10:41 +0530

    js code refactoring

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const introForm = document.querySelector('.intro-form');
 const inputBoxes = introForm.querySelectorAll('.intro-form-input')
+const textMatch = /[a-z]{4,20}/
+const emailMatch = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+const passwordMatch = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
+
+function validateInputValue(inputRef, type) {
+    const inputValue = inputRef.value
+    if(type === 'text' && !textMatch.test(inputValue)) {
+        showError(inputRef)
+    }
+
+    if(type === 'email' && !emailMatch.test(inputValue)) {
+        showError(inputRef)
+    }
+
+    if(type === 'password' && !passwordMatch.test(inputValue)) {
+        showError(inputRef)
+    }
+
+
+}
 
 function showError(node){
     const errorImage = node.querySelector('.intro-form-error-icon')
@@ -18,22 +38,18 @@ function hideErrorMsg(node){
 introForm.addEventListener('submit', function(e){
     e.preventDefault();
     const formContents = this;
-    
-    if(!formContents.firstName.value) {
-        showError(formContents.firstName.parentNode)
-    }
-    
-    if(!formContents.lastName.value) {
-        showError(formContents.lastName.parentNode)
-    }
 
-    if(!formContents.email.value) {
-        showError(formContents.email.parentNode)
-    }
+    // first name validation
+    validateInputValue(formContents.firstName, formContents.firstName.type)
 
-    if(!formContents.password.value) {
-        showError(formContents.password.parentNode)
-    }
+    // last name validation
+    validateInputValue(formContents.lastName, formContents.lastName.type)
+
+    // email validation
+    validateInputValue(formContents.email, formContents.email.type)
+
+    // password validation
+    validateInputValue(formContents.password, formContents.password.type)
 })
 
 inputBoxes.forEach(inputBox => {
