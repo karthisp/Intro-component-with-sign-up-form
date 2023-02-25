94365fbb3715599fc757967097fe6d8b8d01e80d
Karthik Sp <karthik.srinivasprasad@gmail.com>
2023-02-25 11:59:06 +0530

    chanding functin order

--- script.js
+++ script.js
@@ -1,37 +1,6 @@
 const introForm = document.querySelector('.intro-form');
 const inputBoxes = introForm.querySelectorAll('.intro-form-input')
 
-introForm.addEventListener('submit', function(e){
-    e.preventDefault();
-    const formContents = this;
-    
-    if(!formContents.firstName.value) {
-        console.log(formContents.firstName)
-        showError(formContents.firstName.parentNode)
-    }
-    
-    if(!formContents.lastName.value) {
-        console.log(formContents.lastName)
-        showError(formContents.lastName.parentNode)
-    }
-
-    if(!formContents.email.value) {
-        console.log(formContents.email.parentNode)
-        showError(formContents.email.parentNode)
-    }
-
-    if(!formContents.password.value) {
-        console.log(formContents.password)
-        showError(formContents.password.parentNode)
-    }
-})
-
-inputBoxes.forEach(inputBox => {
-    inputBox.addEventListener('focus', function() {
-        hideErrorMsg(this.parentNode)
-    })
-})
-
 function showError(node){
     const errorImage = node.querySelector('.intro-form-error-icon')
     const errorReason = node.querySelector('.intro-form-error-msg')
@@ -45,3 +14,30 @@ function hideErrorMsg(node){
     errorImage.classList.add('intro-hide-elm')
     errorReason.classList.add('intro-hide-elm')
 }
+
+introForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const formContents = this;
+    
+    if(!formContents.firstName.value) {
+        showError(formContents.firstName.parentNode)
+    }
+    
+    if(!formContents.lastName.value) {
+        showError(formContents.lastName.parentNode)
+    }
+
+    if(!formContents.email.value) {
+        showError(formContents.email.parentNode)
+    }
+
+    if(!formContents.password.value) {
+        showError(formContents.password.parentNode)
+    }
+})
+
+inputBoxes.forEach(inputBox => {
+    inputBox.addEventListener('focus', function() {
+        hideErrorMsg(this.parentNode)
+    })
+})
